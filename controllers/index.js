c8988a2473d53c18bac7c85b5b91225107619eab
Joy Rhee <joyfullyx@gmail.com>
2021-04-30 12:05:06 -0700

    add handlebars routes and some handlebars files...

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const { Project } = require('../models');
 
 router.use('/api', apiRoutes);
+
+router.use('/', async (req, res) => {
+    const projects = await Project.findAll();
+    res.json(projects);
+})
 
 module.exports = router;
